2ff833e072866cfb326a600ef2c836642575226d
JhonTabio <JhonTabioCS@gmail.com>
2025-10-30 22:25:54 -0400

    build(core): add ASM + ASMCommons in jar
    Changed MINECRAFTSERVER_NAME to look for the integrated server class name instead of the minecraftserver class name
    (This is because init is abstract in minecraft server and defined in integrated server)
    Changed modifier sanity checks for it
    Event listener for starting a singleplayer integrated server

--- src/main/java/com/jhontabio/minecraftreader/IntegratedServerTransformer.java
+++ src/main/java/com/jhontabio/minecraftreader/IntegratedServerTransformer.java
@@ -30,63 +30,59 @@ public final class IntegratedServerTransformer implements ClassFileTransformer
   public byte[] transform(Module module, ClassLoader loader, String className, 
                           Class<?> classBeingRedefined, ProtectionDomain pd, byte[] buf)
   {
-    // Is class the one we are interested in?
-    if(className == null || className.startsWith("com/jhontabio/minecraftreader/") ||!className.equals(minecraftserver_name)) return null;
-
-    if(loader == null) return null;
-
-    ClassReader cr = new ClassReader(buf);
-    ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-    ClassVisitor cv = new ClassVisitor(ASM9, cw)
-    {
-      @Override
-      public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions)
-      {
-        MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-
-        if((access & (Opcodes.ACC_ABSTRACT | Opcodes.ACC_NATIVE)) != 0) return mv;
-
-        if(!name.equals(minecraftserver_init) && !name.equals(minecraftserver_stop)) return mv;
-
-        return new AdviceAdapter(ASM9, mv, access, name, desc)
-        {
-          private final String owner = className;
-          private final String mName = name;
-          private final String mDesc = desc;
-
-          private Label startLabel = new Label();
-          private Label endLabel = new Label();
-
-          @Override
-          public void visitCode()
-          {
-            super.visitCode();
-            visitLabel(startLabel);
-          }
-
-          @Override
-          protected void onMethodEnter()
-          {
-            visitLdcInsn(owner);
-            visitLdcInsn(mName);
-            visitLdcInsn(mDesc);
-
-            visitMethodInsn(INVOKESTATIC, "com/jhontabio/minecraftreader/Silverfish", "initServer", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", false);
-          }
-        };
-      }
-    };
-
     try
     {
+      // Is class the one we are interested in?
+      if(className == null || className.startsWith("com/jhontabio/minecraftreader/") ||!className.equals(minecraftserver_name)) return null;
+
+      if(loader == null) return null;
+
+      ClassReader cr = new ClassReader(buf);
+      ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+      ClassVisitor cv = new ClassVisitor(ASM9, cw)
+      {
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions)
+        {
+          MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
+
+          if((access & (Opcodes.ACC_ABSTRACT | Opcodes.ACC_NATIVE)) != 0) return mv;
+
+          if(!name.equals(minecraftserver_init) && !name.equals(minecraftserver_stop)) return mv;
+
+          return new AdviceAdapter(ASM9, mv, access, name, desc)
+          {
+            private final String owner = className;
+            private final String mName = name;
+            private final String mDesc = desc;
+
+            private Label startLabel = new Label();
+            private Label endLabel = new Label();
+
+            @Override
+            public void visitCode()
+            {
+              super.visitCode();
+              visitLabel(startLabel);
+            }
+
+            @Override
+            protected void onMethodEnter()
+            {
+              Silverfish.SILVERFISH_STREAM.println("MehtodEnter");
+              visitLdcInsn(owner);
+              visitLdcInsn(mName);
+              visitLdcInsn(mDesc);
+
+              visitMethodInsn(INVOKESTATIC, "com/jhontabio/minecraftreader/Silverfish", "initServer", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", false);
+            }
+          };
+        }
+      };
+
       cr.accept(cv, ClassReader.EXPAND_FRAMES);
       return cw.toByteArray();
     }
-    // Fallback to original byte sequence
-    catch(Throwable t)
-    {
-      t.printStackTrace();
-      return null;
-    }
+    catch(Throwable t){ return buf; }
   }
 }
